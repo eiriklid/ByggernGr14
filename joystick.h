27a7078c567cb0fbf276aab9177518307a305e59
eiriklid <eiriklid@gmail.com>
2015-09-12 10:48:14 +0200

    Create joystick.h

---
+++ joystick.h
@@ -0,0 +1,17 @@
+/*
+ * joystick.h
+ *
+ * Created: 10.09.2015 15:46:49
+ *  Author: eiriklid
+ */ 
+
+
+#ifndef JOYSTICK_H_
+#define JOYSTICK_H_
+
+int digital2Prosent(int val);
+int JOY_x_pos();
+int JOY_y_pos();
+
+
+#endif /* JOYSTICK_H_ */
