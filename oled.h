47b275f452df5286f1d0df4660798c3d6e8c2cb4
solveds <s_dahlin7@hotmail.com>
2015-09-17 15:58:35 +0200

    Create oled.h

---
+++ oled.h
@@ -0,0 +1,21 @@
+/*
+ * oled.h
+ *
+ * Created: 17.09.2015 14:15:48
+ *  Author: solveds
+ */ 
+
+
+#ifndef OLED_H_
+#define OLED_H_
+
+void write_c(char command);
+void write_d(char data);
+
+void OLED_init();
+void OLED_reset();
+
+
+
+
+#endif /* OLED_H_ */
