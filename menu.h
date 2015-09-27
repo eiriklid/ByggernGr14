6d63515e2cefa21d4c956fecef29539d7fa18b19
eiriklid <eiriklid@gmail.com>
2015-09-27 14:44:56 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -10,6 +10,22 @@
 #define MENU_H_
 
 
+typedef struct MenuNode {
+	
+	struct MenuNode *next;
+	struct MenuNode *prev;
+	struct MenuNode *parent;
+	struct MenuNode *child;
+	
+	
+	char* name;
+	
+} MenuNode;
+
+
+MenuNode* menu_init(char *name);
+void menu_insert_submenu(MenuNode *parent, char* name);
+void menu_insert_node(MenuNode* next, MenuNode* prev, char* name);
 
 
 
