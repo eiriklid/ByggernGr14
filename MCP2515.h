379feab6738135f2b8a98b50d5a9c87969bd7efa
solveds <s_dahlin7@hotmail.com>
2015-10-01 16:23:18 +0200

    Update MCP2515.h

--- MCP2515.h
+++ MCP2515.h
@@ -155,5 +155,16 @@ Copyright 2003 Kimberly Otten Software Consulting
 #define MCP_MERRF		0x80
 
 
+//Edited by us
+
+uint8_t mcp2515_read(uint8_t address);
+void mcp2515_write(uint8_t address, uint8_t data);
+void mcp2515_request_to_send();											//Sends request to all, can be modified to request specific 
+uint8_t mcp2515_read_status();
+void mcp2515_bit_modify(uint8_t address, uint8_t mask, uint8_t data);
+void mcp2515_reset();
+
+
+
 
 #endif
