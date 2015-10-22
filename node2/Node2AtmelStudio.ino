9792aab5d2189bae9f3c16011d635ab7cb2e444c
eiriklid <eiriklid@gmail.com>
2015-10-22 12:41:05 +0200

    Update Node2AtmelStudio.ino

--- node2/Node2AtmelStudio.ino
+++ node2/Node2AtmelStudio.ino
@@ -7,63 +7,71 @@
 
 void setup()
 {
-	Serial.begin(9600);
-	CAN_init();
-	
+  Serial.begin(9600);
+  CAN_init();
+  
 
 }
 
 void loop()
 {
-	
-	/*
-	SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-	
-	SPI.transfer(0x00);
-	int j = SPI.transfer(0xAA);
-	Serial.print(j,HEX);
-	
-	SPI.endTransaction();
-	*/
-	
-	/*
-	mcp2515_reset();
-	mcp2515_write(0x36, 0x01);
-	int i = mcp2515_read(0x66);
-	Serial.print(i);
-	*/
-	
-	//Test for CAN - send & receive msg
-	
-	
-	can_message_t can_msg;
-	
-	can_msg.id = 1;
-	can_msg.length = 2;
-	can_msg.data[0] = 187;
-	can_msg.data[1] = 6;
-	
-	CAN_message_send(&can_msg);
-	delay(100);
-	
-		
-	can_message_t can_receive = CAN_data_receive();
+  
+  /*
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  
+  SPI.transfer(0x00);
+  int j = SPI.transfer(0xAA);
+  Serial.print(j,HEX);
+  
+  SPI.endTransaction();
+  */
+  
+  /*
+  mcp2515_reset();
+  mcp2515_write(0x36, 0x01);
+  int i = mcp2515_read(0x66);
+  Serial.print(i);
+  */
+  
+  //Test for CAN - send & receive msg
+  
+  
+  can_message_t can_msg;
+  
+  can_msg.id = 4;
+  can_msg.length = 2;
+  can_msg.data[0] = 185;
+  can_msg.data[1] = 4;  
+  can_msg.data[2] = 9;
 
-	Serial.print("ID: ");
-	Serial.print(can_receive.id);
+   
+  CAN_message_send(&can_msg);
+  
+  delay(100);
+  
+  
+ /* can_message_t can_receive = CAN_data_receive();
 
-	Serial.print("\n");
-	Serial.print(can_receive.data[0]);
-	
-	Serial.print("\n");
-	Serial.print(can_receive.data[1]);
+  Serial.print("ID:\t ");
+  Serial.print(can_receive.id, BIN);
 
-	Serial.print("\n");
-	Serial.print(can_receive.length);
-	
-	Serial.print("\n\n");
-	
-	
-	delay(100);
+  Serial.print("\nLenght:\t  ");
+  Serial.print(can_receive.length);
+  
+  Serial.print("\nData:\t");
+  Serial.print(can_receive.data[0]);
+  
+  Serial.print("\n\t");
+  Serial.print(can_receive.data[1]);
+  
+  Serial.print("\n\t");
+  Serial.print(can_receive.data[2]);
+*/
+  
+  Serial.print("\n\n");
+  Serial.println(mcp2515_read(MCP_CANINTF), BIN);
+  
+  
+  delay(100);
   
 }
