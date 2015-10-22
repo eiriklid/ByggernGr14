1ee2f7ea01b03df21d398c7aed24e422f8c7cfae
eiriklid <eiriklid@gmail.com>
2015-10-22 12:42:50 +0200

    Update can.c

--- can.c
+++ can.c
@@ -18,6 +18,8 @@ void CAN_init(){
 }
 
 void CAN_message_send(can_message_t* can_msg){
+	//mcp2515_write( MCP_CANINTF, 0);
+	
 	uint8_t low_id = ( ((can_msg->id) & (0b111))<< 5); 
 	uint8_t high_id = ( (can_msg->id) >> 3);
 	
@@ -32,22 +34,26 @@ void CAN_message_send(can_message_t* can_msg){
 	}
 	
 	mcp2515_request_to_send(0);
+	printf("%02X\n", mcp2515_read(MCP_CANINTF));
 	
 }
 
 can_message_t CAN_data_receive(){
 	
+	mcp2515_write( MCP_CANINTF, 0);
 	
+	mcp2515_bit_modify(MCP_CANINTF, MCP_RX0IF, 0);
 	uint8_t high_adr = mcp2515_read(MCP_RXB0SIDH);
 	uint8_t low_adr = mcp2515_read(MCP_RXB0SIDL);
 	
 	can_message_t return_msg;
 	return_msg.id = ( (high_adr<<3) | (low_adr >>5) );
-	return_msg.length = mcp2515_read(MCP_RXB0DLC);
+	return_msg.length = mcp2515_read(MCP_RXB0DLC) & 0x0F;
 	
 	for (uint8_t i = 0; i < return_msg.length; i++){
 		
 		return_msg.data[i] = mcp2515_read(MCP_RXB0D0 + i);
+		printf("%d\n", i);
 		
 	}
 	
