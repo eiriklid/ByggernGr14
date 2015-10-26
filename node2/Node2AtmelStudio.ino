c475184ec38578c18e7c93ced935eafbf379ae42
eiriklid <eiriklid@gmail.com>
2015-10-26 16:00:16 +0100

    Update Node2AtmelStudio.ino

--- node2/Node2AtmelStudio.ino
+++ node2/Node2AtmelStudio.ino
@@ -1,77 +1,175 @@
-#include "can.h"
+/*
+ * main.c
+ *
+ * Created: 10.09.2015 08:38:46
+ *  Author: eiriklid
+ */ 
+
+
+//#define FOSC 1843200// Clock Speed
+#define F_CPU 4915200// Clock Speed
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+
+#include "usart.h"
+#include "macros.h"
+#include "sram.h"
+#include "joystick.h"
+#include "oled.h"
 #include "MCP2515.h"
+#include "spi.h"
+#include "can.h"
 
-#include "Arduino.h"
-#include "SPI.h"
 
-
-void setup()
+int main(void)
 {
-  Serial.begin(9600);
-  CAN_init();
-  
+	
+	
+	DDRB |= (1<<PB0);
+	USART_Init(31);
+	
+	MCUCR |= (1 << SRE);
+	fdevopen( USART_Transmit, USART_Receive);
+	//SRAM_test();
+	
+	JOY_init();
+	
+	SPI_MasterInit();
+	
+	OLED_init();
+	OLED_reset();
+	
+	OLED_menu();
+	
+	CAN_init();
+	
+	
+	
+    while(1)
+    {
+		set_bit(PORTB,PB0);
+		
+		
+        //Output-test ex.1
+		/*
+		PORTA |=(1<<PA0);
+		_delay_ms(500);              
+		PORTA &= ~(1<<PA0);
+		_delay_ms(500);              
+		*/
+		
+		// ex.1
+		/*
+		//USART_Transmit('a');
+		usart_char = USART_Receive();
+		USART_Transmit(usart_char);
+		*/
+		
+		//ex.2
+		/*
+		clear_bit(PORTE,PE1);
+		set_bit(PORTA,PA0);
+		set_bit(PORTE,PE1);		
+		_delay_ms(500);
+		
+		clear_bit(PORTE,PE1);
+		set_bit(PORTA,PA1);
+		set_bit(PORTE,PE1);
+		_delay_ms(500);
+		
+		clear_bit(PORTE,PE1);
+		clear_bit(PORTA,PA0);
+		clear_bit(PORTA,PA1);
+		set_bit(PORTE,PE1);
+		_delay_ms(500);
+		*/
+		
 
-}
+		
+		
+		/*
+		printf("X er: %d \t Y er: %d \n", JOY_x_pos(),JOY_y_pos());
+		
+		//printf("Slider left: %d \t Slider right %d \n", JOY_slider(1), JOY_slider(0) );
+		printf("Direction: %d \n", JOY_direction( JOY_x_pos(), JOY_y_pos()));
+		*/
+		
+		
+		
+		
+		joy_dir direction = JOY_direction(JOY_x_pos(), JOY_y_pos());
+		if (direction == DOWN){
+			OLED_move_arrow(3);
+			
+		}
+		if (direction == UP){			
+			OLED_move_arrow(1);
 
-void loop()
-{
-  
-  /*
-  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-  
-  SPI.transfer(0x00);
-  int j = SPI.transfer(0xAA);
-  Serial.print(j,HEX);
-  
-  SPI.endTransaction();
-  */
-  
-  /*
-  mcp2515_reset();
-  mcp2515_write(0x36, 0x01);
-  int i = mcp2515_read(0x66);
-  Serial.print(i);
-  */
-  
-  //Test for CAN - send & receive msg
-  
-  
-  can_message_t can_msg;
-  
-  can_msg.id = 4;
-  can_msg.length = 2;
-  can_msg.data[0] = 185;
-  can_msg.data[1] = 4;  
-  can_msg.data[2] = 9;
-
-   
-  CAN_message_send(&can_msg);
-  
-  delay(100);
-  
-  
- /* can_message_t can_receive = CAN_data_receive();
-
-  Serial.print("ID:\t ");
-  Serial.print(can_receive.id, BIN);
-
-  Serial.print("\nLenght:\t  ");
-  Serial.print(can_receive.length);
-  
-  Serial.print("\nData:\t");
-  Serial.print(can_receive.data[0]);
-  
-  Serial.print("\n\t");
-  Serial.print(can_receive.data[1]);
-  
-  Serial.print("\n\t");
-  Serial.print(can_receive.data[2]);
-*/
-  
-  Serial.print("\n\n");
-  Serial.println(mcp2515_read(MCP_CANINTF), BIN);
-  
-  
-  delay(100);
-  
+		}
+		if (direction == RIGHT){
+			OLED_print_submenu();
+		}
+		if (direction == LEFT){
+			OLED_print_parentmenu();
+		}
+		
+		
+		
+		/*
+		printf("Left: %d \n",JOY_button(1));
+		printf("Right: %d \n",JOY_button(0));
+		*/
+		
+		
+		
+		
+		//Test for SPI, short circuit MISO & MOSI 
+		/*
+		SPI_send(0x4a);
+		volatile uint8_t i = SPI_read();
+		*/
+		
+		/*
+		mcp2515_reset();
+		
+		mcp2515_write(0x36, 0xAA);
+		
+		printf("CANSTAT  0x%02x \n", mcp2515_read(0x36));
+		*/
+		
+		//Test for CAN - send & receive msg
+		can_message_t can_msg;
+		
+		//can_msg.id = 0b11000101010;
+		can_msg.id = 19;
+		can_msg.length = 2;
+		can_msg.data[0] = 7;
+		can_msg.data[1] = 7;
+		can_msg.data[2] = 85;
+		
+		
+		CAN_message_send(&can_msg);
+		
+		//printf("%d \n", mcp2515_read(0x66) );
+		//printf("%d \n", mcp2515_read(0x67) );
+		/*
+		can_message_t can_receive = CAN_data_receive();
+		
+		printf("ID: %d \n", can_receive.id);
+		printf("Length: %d \n", can_receive.length);
+		printf("Data: %d \n", can_receive.data[0]);
+		printf("%d \n", can_receive.data[1]);
+		printf("%d \n\n", can_receive.data[2]);
+		*/
+		
+		
+		_delay_ms(50);
+		clear_bit(PORTB,PB0);
+		_delay_ms(50);
+    }
 }
