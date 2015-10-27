abbbc7e104ab89129f1512a80bd2be23d625337d
eiriklid <eiriklid@gmail.com>
2015-10-27 16:07:42 +0100

    Update can.cpp

--- node2/can.cpp
+++ node2/can.cpp
@@ -26,7 +26,7 @@ void CAN_message_send(can_message_t* can_msg){
   
   for (uint8_t i = 0 ; i < can_msg->len ; i++)
   {
-    mcp2515_write(MCP_TXB0D0 + i, can_msg->data[i] );
+    mcp2515_write(MCP_TXB0D0 + i, can_msg->data[i] ); 
   }
   
   mcp2515_request_to_send(0);
@@ -36,22 +36,22 @@ void CAN_message_send(can_message_t* can_msg){
 can_message_t CAN_data_receive(){
 	
   //mcp2515_bit_modify(MCP_CANINTF, MCP_RX0IF,0);
-  //mcp2515_write( MCP_CANINTF, 0);
+  mcp2515_write( MCP_CANINTF, 0);
 	uint8_t high_adr = mcp2515_read(MCP_RXB0SIDH);
 	uint8_t low_adr = mcp2515_read(MCP_RXB0SIDL);
 	
 	can_message_t return_msg;
 	return_msg.id = ( (high_adr<<3) | (low_adr >>5) );
-  Serial.println(return_msg.len);
+  //Serial.println(return_msg.len);
 	return_msg.len = mcp2515_read(MCP_RXB0DLC);
-	Serial.println(return_msg.len);
+	//Serial.println(return_msg.len);
 	for (uint8_t i = 0; i < return_msg.len; i++){
 		
 		return_msg.data[i] = mcp2515_read(MCP_RXB0D0 + i);
 		
 	}
 	
-  Serial.println("Point 1");
+
 	return return_msg;
   
   
