cf418d691c091b75d3ca252685a3ca8b8c826056
eiriklid <eiriklid@gmail.com>
2015-11-17 17:11:35 +0100

    Create highscore.h

---
+++ node1/highscore.h
@@ -0,0 +1,19 @@
+/*
+ * highscore.h
+ *
+ * Created: 17.11.2015 14:14:37
+ *  Author: eiriklid
+ */ 
+
+
+#ifndef HIGHSCORE_H_
+#define HIGHSCORE_H_
+
+void highscore_init();
+void highscore_add(uint16_t score);
+uint16_t highscore_get(uint8_t index);
+
+
+
+
+#endif /* HIGHSCORE_H_ */
