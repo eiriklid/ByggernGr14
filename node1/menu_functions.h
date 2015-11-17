07347849ab9ad0885a64ca6efbce416c5195daf6
eiriklid <eiriklid@gmail.com>
2015-11-17 17:14:36 +0100

    Create menu_functions.h

---
+++ node1/menu_functions.h
@@ -0,0 +1,18 @@
+/*
+ * menu_functions.h
+ *
+ * Created: 17.11.2015 11:49:37
+ *  Author: eiriklid
+ */ 
+
+
+#ifndef MENU_FUNCTIONS_H_
+#define MENU_FUNCTIONS_H_
+
+void OLED_addjust_brightness();
+void play_game();
+void highscore_reset();
+void OLED_print_highscore();
+
+
+#endif /* MENU_FUNCTIONS_H_ */
