2586882b62a8f728a9159b724bfc67cfc88f0132
eiriklid <eiriklid@gmail.com>
2015-11-18 23:51:00 +0100

    Update menu_functions.h

--- node1/menu_functions.h
+++ node1/menu_functions.h
@@ -8,12 +8,24 @@
 
 #ifndef MENU_FUNCTIONS_H_
 #define MENU_FUNCTIONS_H_
+//These functions are used in menu.h as function pointers in MenuNode->node_func
+
+//CAN_message id:	1 = Start game 
+//					2 = Game over
+//					3 = Lose life
+//					4 = Controller input
+//					5 = Node 2 response of starting game
+
 
 void menu_func_OLED_addjust_brightness();
 void menu_func_play_game();
 void menu_func_highscore_reset();
 void menu_func_OLED_print_highscore();
-void menu_func_OLED_game_print(uint8_t lives, uint16_t time, char* game_status);
+void menu_func_OLED_game_print(uint8_t lives, uint32_t time, char* game_status);
+
+void menu_func_choose_joystick();
+void menu_func_choose_slider();
+void menu_func_choose_ultrasonic();
 
 
 
