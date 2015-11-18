cd6b12bf3b033558269bc85b98fbae5795290531
eiriklid <eiriklid@gmail.com>
2015-11-18 17:51:13 +0100

    Update menu_functions.h

--- node1/menu_functions.h
+++ node1/menu_functions.h
@@ -9,10 +9,12 @@
 #ifndef MENU_FUNCTIONS_H_
 #define MENU_FUNCTIONS_H_
 
-void OLED_addjust_brightness();
-void play_game();
-void highscore_reset();
-void OLED_print_highscore();
+void menu_func_OLED_addjust_brightness();
+void menu_func_play_game();
+void menu_func_highscore_reset();
+void menu_func_OLED_print_highscore();
+void menu_func_OLED_game_print(uint8_t lives, uint16_t time, char* game_status);
+
 
 
 #endif /* MENU_FUNCTIONS_H_ */
